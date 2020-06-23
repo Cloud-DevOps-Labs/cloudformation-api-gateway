c85007e83fd7a6c18acd90d74d66feccc30ac9fb
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2020-06-23 13:10:45 +0200

    Update integration-test.sh

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+npm --version >/dev/null 2>&1 || { echo >&2 "I require npm as dependency package manager but it's not installed. ¯\_(ツ)_/¯ Aborting."; exit 1; }
+
 # Executes integration tests against version $LATEST
 
 set -e
